98acd3d606fb291bf4c4fa2cccc259741ac7bf08
yanhaoqi <yanhaoqi2008@foxmail.com>
2018-01-02 17:53:41 +0800

    1

---
+++ vuetest/src/components/tree/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <li>
+      <span v-text="data.title"></span>
+      <ul v-if="data.children && data.children.length > 0">
+        <!--在自身组件中调用自身 通过动态属性把数据传递给被调用的自身-->
+        <treeList v-for="(item, index) in data.children" :data="item" :key="index"></treeList>
+      </ul>
+    </li>
+  </section>
+</template>
+<script>
+  export default{
+    name: 'treeList',
+    props: {
+      data: {
+        type: Object,
+        require: true
+      }
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  ul{
+    list-style: none;
+    margin: 10px;
+  }
+</style>
---
+++ vuetest/src/views/home.vue
@@ -0,0 +1,96 @@
+<template>
+  <section>
+    <h1>这是vuetest的主页</h1>
+    <div class="tree">
+      <ul v-for="item in cascader">
+        <tree :data="item"></tree>
+      </ul>
+    </div>
+  </section>
+</template>
+<script>
+  import tree from '../components/tree'
+  export default{
+    components: {
+      tree
+    },
+    data () {
+      return {
+        cascader: [
+          {
+            title: 'A',
+            children: [
+              {
+                title: 'A -[ 1 ]',
+                children: [
+                  {
+                    title: 'A -[ 1 ]- 1'
+                  },
+                  {
+                    title: 'A -[ 1 ]- 2'
+                  }
+                ]
+              },
+              {
+                title: 'A -[ 2 ]',
+                children: [
+                  {
+                    title: 'A -[ 2 ]- 1'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            title: 'B',
+            children: [
+              {
+                title: 'B -[ 1 ]',
+                children: [
+                  {
+                    title: 'B -[ 1 ]- 1'
+                  },
+                  {
+                    title: 'B -[ 1 ]- 2'
+                  }
+                ]
+              },
+              {
+                title: 'B -[ 2 ]',
+                children: [
+                  {
+                    title: 'B -[ 2 ]- 1'
+                  },
+                  {
+                    title: 'B -[ 2 ]- 2'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            title: 'C',
+            children: [
+              {
+                title: 'C -[ 1 ]'
+              },
+              {
+                title: 'C -[ 2 ]'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  section{
+    h1{
+      margin: 30px auto;
+    }
+    .tree{
+
+    }
+  }
+</style>
